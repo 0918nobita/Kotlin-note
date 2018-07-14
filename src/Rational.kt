de5453fe5983d136900f23d34e676ab33c4551f3
0918nobita <nobita.0918@gmail.com>
2018-07-15 06:18:38 +0900

    Rational クラスで、インスタンス生成時に自動で約分する機能を追加した

--- src/Rational.kt
+++ src/Rational.kt
@@ -1,7 +1,21 @@
 package sample
 
 // プライマリコンストラクタ
-class Rational(private val numerator: Int,
-               private val denominator: Int) {
+class Rational(n: Int, d: Int) {
+  // セカンダリコンストラクタ
+  init {
+    require(d != 0, {"denominator must not be null"})
+  }
+
+  private val g = gcd(Math.abs(n), Math.abs(d))
+
+  private val numerator: Int = n / g
+  private val denominator: Int = n / g
+
   override fun toString(): String = "$numerator/$denominator"
+
+  // tailrec: 末尾再帰
+  private tailrec fun gcd(a: Int, b: Int): Int =
+    if (b == 0) a
+    else gcd(b, a % b)
 }
