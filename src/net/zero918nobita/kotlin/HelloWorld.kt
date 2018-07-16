28340c1c0bd2da4f6f3b63789608c3f04fe1d312
0918nobita <nobita.0918@gmail.com>
2018-07-16 09:25:27 +0900

    Add examples of `run {...}` & arrayOfNulls

--- src/net/zero918nobita/kotlin/HelloWorld.kt
+++ src/net/zero918nobita/kotlin/HelloWorld.kt
@@ -7,4 +7,12 @@ fun main(args: Array<String>) {
   println(half + oneThird)  // => "5/6"
   println(half + 2)         // => "5/2"
   println(2 + half)         // => "5/2"
+
+  run {
+    val ints = arrayOfNulls<Int>(5)
+    println("ints[0]: ${ints[0]}, ints.size: ${ints.size}")
+
+    ints[0] = 123
+    println("ints[0]: ${ints[0]}")
+  }
 }
