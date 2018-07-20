f05c2ed970e30d4542ddc93301b0e7748ddc2bbc
0918nobita <nobita.0918@gmail.com>
2018-07-20 19:54:06 +0900

    Update Function.kt

--- src/net/zero918nobita/kotlin/Function.kt
+++ src/net/zero918nobita/kotlin/Function.kt
@@ -1,8 +1,13 @@
 package net.zero918nobita.kotlin
 
 fun succ(i: Int) = i + 1
+fun square(i: Int) = i * i
+fun hello(name: String) = "Hello, $name!"
+fun max(a: Int, b: Int) = if (b <= a) a else b
 
 fun main(args: Array<String>) {
-  val result = succ(31)
-  println(result)
+  println(succ(30))  // => 31
+  println(square(5))  // => 25
+  println(hello("Kotlin"))  // => "Hello, Kotlin"
+  println(max(1, 2))  // => 1
 }
