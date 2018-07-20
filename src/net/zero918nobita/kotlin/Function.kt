e5bfb69d71f4c6fc1eb80ed31c520d8de31f2360
0918nobita <nobita.0918@gmail.com>
2018-07-20 20:14:06 +0900

    Add examples of default arguments

--- src/net/zero918nobita/kotlin/Function.kt
+++ src/net/zero918nobita/kotlin/Function.kt
@@ -2,7 +2,9 @@ package net.zero918nobita.kotlin
 
 fun succ(i: Int) = i + 1
 fun square(i: Int) = i * i
-fun hello(name: String) = "Hello, $name!"
+
+fun hello(name: String = "World") = "Hello, $name!"
+
 fun max(a: Int, b: Int) = if (b <= a) a else b
 
 fun sum(ints: Array<Int>): Int {
@@ -16,10 +18,12 @@ fun sub(minuend: Int, subtrahend: Int) = minuend - subtrahend
 fun main(args: Array<String>) {
   println(succ(30))  // => 31
   println(square(5))  // => 25
-  println(hello("Kotlin"))  // => "Hello, Kotlin"
   println(max(1, 2))  // => 1
   println(sum(arrayOf(1, 2, 3)))  // => 6
 
   println(sub(minuend = 10, subtrahend = 7))  // => 3
   println(sub(subtrahend = 7, minuend = 10))  // => 3
+
+  println(hello("Kotlin"))  // => "Hello, Kotlin"
+  println(hello())  // => "Hello, Kotlin"
 }
