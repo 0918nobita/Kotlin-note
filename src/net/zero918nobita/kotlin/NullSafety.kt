f9c0f6c57e4f39190b67eaac66ea7a20595f32d1
0918nobita <nobita.0918@gmail.com>
2018-08-12 23:16:36 +0900

    スマートキャストを利用したサンプルを追加した

--- src/net/zero918nobita/kotlin/NullSafety.kt
+++ src/net/zero918nobita/kotlin/NullSafety.kt
@@ -5,4 +5,12 @@ fun main(args: Array<String>) {
 
   // val foo: String? = null  // OK
   // println(foo!!.length)  // NullPointerException
+
+  val ints: List<Int> = mutableListOf(1, 2, 3)
+
+  // if (ints is MutableList<Int>) (ints as MutableList<Int>).add(4)
+  if (ints is MutableList<Int>) {
+    ints.add(4)
+  }
+  println(ints)  // => [1, 2, 3, 4]
 }
