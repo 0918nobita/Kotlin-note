8369d41b7766cb86cb453ab5ada41bde1df31d3a
0918nobita <nobita.0918@gmail.com>
2018-08-12 18:11:35 +0900

    継承とオーバーライドを用いたサンプルを追加した

--- src/net/zero918nobita/kotlin/Person.kt
+++ src/net/zero918nobita/kotlin/Person.kt
@@ -1,11 +1,19 @@
 package net.zero918nobita.kotlin
 
-class Person(private val name: String) {
-  fun show() = println(name)
+open class Person(private val name: String) {
+  open fun show() = println(name)
+}
+
+class Student(private val id: Long,
+              private val name: String): Person(name) {
+  override fun show() = println("Student(id=$id, name=$name)")
 }
 
 fun main(args: Array<String>) {
   val constructor = ::Person
   val taro = constructor("Taro")
   taro.show()  // => Taro
+
+  val student = Student(123, "Tom")
+  student.show()
 }
