76a3f15312d6b7e69476d0398d34de37d9cf753a
0918nobita <nobita.0918@gmail.com>
2018-08-12 17:59:00 +0900

    プライマリコントラクタを用いたサンプルを追加した

--- src/net/zero918nobita/kotlin/Person.kt
+++ src/net/zero918nobita/kotlin/Person.kt
@@ -1,12 +1,11 @@
 package net.zero918nobita.kotlin
 
-class Person {
-  var name = ""
+class Person(n: String) {
+  var name: String = n
 }
 
 fun main(args: Array<String>) {
   val constructor = ::Person
-  val taro = constructor()
-  taro.name = "Taro"
+  val taro = constructor("Taro")
   println(taro.name)  // => Taro
 }
