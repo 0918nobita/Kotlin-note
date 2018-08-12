642257a74294be3173bf23e11dfc63195c49ac39
0918nobita <nobita.0918@gmail.com>
2018-08-12 22:24:39 +0900

    拡張プロパティを用いたサンプルを追加した

--- src/net/zero918nobita/kotlin/Extension.kt
+++ src/net/zero918nobita/kotlin/Extension.kt
@@ -1,8 +1,13 @@
 package net.zero918nobita.kotlin
 
-fun Int.isOdd(): Boolean = this % 2 == 0
+// fun Int.isOdd(): Boolean = (this % 2 == 0)
+
+val Int.isOdd: Boolean
+  get() = (this % 2 != 0)
 
 fun main(args: Array<String>) {
-  println(5.isOdd())
-  println(6.isOdd())
+  // println(5.isOdd())
+  // println(6.isOdd())
+  println(5.isOdd)
+  println(6.isOdd)
 }
