358113aa388b22d6ede5dcefa115c431fe3c1219
0918nobita <nobita.0918@gmail.com>
2018-08-12 23:16:26 +0900

    NotNull、Nullableを用いたサンプルを追加した

---
+++ src/net/zero918nobita/kotlin/NullSafety.kt
@@ -0,0 +1,8 @@
+package net.zero918nobita.kotlin
+
+fun main(args: Array<String>) {
+  // val foo: String = null  // Type Error
+
+  // val foo: String? = null  // OK
+  // println(foo!!.length)  // NullPointerException
+}
