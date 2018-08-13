f1f7fdc29fd2f627f215bca6570cc3506144f98f
0918nobita <nobita.0918@gmail.com>
2018-08-13 09:51:06 +0900

    コンストラクタを通じて得たGreeter2インスタンスを用いて、sayHelloメソッドの呼び出し回数を記録するCountingGreeterクラスを追加した

--- src/net/zero918nobita/kotlin/Delegate.kt
+++ src/net/zero918nobita/kotlin/Delegate.kt
@@ -14,3 +14,26 @@ class EnglishGreeter2(override val name: String): Greeter2 {
 
   override fun sayHello() = println("Hello!")
 }
+
+class CountingGreeter(private val greeter: Greeter2): Greeter2 {
+  var helloCount: Int = 0
+    private set
+
+  override val name: String
+    get() = greeter.name
+
+  override fun introduceMyself() = greeter.introduceMyself()
+
+  override fun sayHello() {
+    helloCount++
+    greeter.sayHello()
+  }
+}
+
+fun main(args: Array<String>) {
+  val countingGreeter = CountingGreeter(EnglishGreeter2("Tom"))
+  countingGreeter.sayHello()
+  countingGreeter.sayHello()
+  countingGreeter.sayHello()
+  println(countingGreeter.helloCount)
+}
